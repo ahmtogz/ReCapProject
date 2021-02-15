e1eb557e966d95f42189a8526e20f1ea60747450
ahmtogz <77540818+ahmtogz@users.noreply.github.com>
2021-02-15 22:58:26 +0100

    15.02.2021

--- Business/Constans/Messages.cs
+++ Business/Constans/Messages.cs
@@ -10,5 +10,21 @@ namespace Business.Constans
         public static string BrandNameInvalid = "Marka ismi geçersiz";
         public static string MaintenanceTime = "Sistem bakımda";
         public static string CarsListed = "Arabalar listelendi";
+
+        public static string AddedCustomer = "Müşteri başarıyla eklendi.";
+        public static string DeletedCustomer = "Müşteri başarıyla silindi.";
+        public static string UpdatedCustomer = "Müşteri başarıyla güncellendi.";
+
+
+        public static string AddedUser = "Kullanıcı başarıyla eklendi.";
+        public static string DeletedUser = "Kullanıcı başarıyla silindi.";
+        public static string UpdatedUser = "Kullanıcı başarıyla güncellendi.";
+
+
+        public static string AddedRental = "Araba Kiralama işlemi baraşıyla gerçekleşti.";
+        public static string DeletedRental = "Araba Kiralama işlemi iptal edildi.";
+        public static string UpdatedRental = "Araba Kiralama işlemi güncellendi.";
+        public static string FailedRentalAddOrUpdate = "Bu araba henüz teslim edilmediği için kiralayamazsınız.";
+        public static string ReturnedRental = "Kiraladığınız araç teslim edildi.";
     }
 }
--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -13,16 +13,24 @@ namespace DataAccess.Concrete.EntityFramework
 {
     public class EfCarDal : EfEntityRepositoryBase<Car, RentACarSqlContext>, ICarDal
     {
-        public List<CarDetailDto> GetCarDetails()
+        public List<CarDetailDto> GetCarDetails(Expression<Func<Car, bool>> filter = null)
         {
             using (RentACarSqlContext context = new RentACarSqlContext())
             {
-                var result = from c in context.Cars
-                             join b in context.Brands
-                             on c.BrandId equals b.BrandId
+                var result = from c in filter == null ? context.Cars : context.Cars.Where(filter)
                              join co in context.Colors
                              on c.ColorId equals co.ColorId
-                             select new CarDetailDto { CarId = c.CarId, BrandName = b.BrandName, ColorName = co.ColorName, DailyPrice = c.DailyPrice };
+                             join b in context.Brands
+                             on c.BrandId equals b.BrandId
+                             select new CarDetailDto
+                             {
+                                 CarId = c.CarId,
+                                 BrandName = b.BrandName,
+                                 ColorName = co.ColorName,
+                                 DailyPrice = c.DailyPrice,
+                                 Descriptions = c.Description,
+                                 ModelYear = c.ModelYear
+                             };
                 return result.ToList();
             }
         }
